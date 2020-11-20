423ab1ce86de270e9cf68471c7b7d3ce5287ba1f
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-20 21:26:56 +0530

    Spring REST- Retrieve single student using path variable

--- src/main/java/org/bhavin/springdemo/rest/StudentRestController.java
+++ src/main/java/org/bhavin/springdemo/rest/StudentRestController.java
@@ -3,8 +3,11 @@ package org.bhavin.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.bhavin.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class StudentRestController {
 	
+	private List<Student> theStudent;
+	
+	// define @PostContruct to load the student data... only once
+	
+	@PostConstruct
+	public void loadData() {
+		
+		theStudent = new ArrayList<>();
+		theStudent.add(new Student("Wayne","Rooney"));
+		theStudent.add(new Student("Cristiano","Ronaldo"));
+		theStudent.add(new Student("Lionel","Messi"));
+		
+	}
+	
 	// define end point for '/students' to return list of students
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		
-		List<Student> tempStudent = new ArrayList<>();
-		tempStudent.add(new Student("Wayne","Rooney"));
-		tempStudent.add(new Student("Cristiano","Ronaldo"));
-		tempStudent.add(new Student("Lionel","Messi"));
+		return theStudent;
+	}
+	
+	// define end point for single student
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
 		
-		return tempStudent;
+		return theStudent.get(studentId);
 	}
 	
 }
