bf4dc641f14407ca0e87a3356795c0adf32beaf8
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-23 18:17:03 +0530

    Spring REST: Implemented Exception Handling- Added Generic Exception Handler

--- src/main/java/org/bhavin/springdemo/rest/StudentRestController.java
+++ src/main/java/org/bhavin/springdemo/rest/StudentRestController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.bhavin.springdemo.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +43,48 @@ public class StudentRestController {
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
 		
+		if((studentId < 0) || (studentId >= theStudent.size()) ) {
+			throw new StudentNotFoundException("Student id: "+studentId+" not found");
+			
+		}
+		
 		return theStudent.get(studentId);
 	}
 	
+	// add an exception handler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException exc) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	// another exception handler to catch any exception
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException (Exception exc) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
